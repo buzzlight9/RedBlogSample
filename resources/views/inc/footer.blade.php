0e0095e906695c25ec336ce1416a812af159e428
Laitia Matagavui <lcavukali@yahoo.com>
2018-06-16 19:43:10 +0100

    favicon

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -5,9 +5,10 @@
 				<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fredblog.cf%2Fabout%2F&layout=button_count&size=large&mobile_iframe=false&width=84&height=28&appId" width="84" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 			</div>
 			<div class="col-sm-4">
-				<a href="/"><img src="../img/logo3.png" class="icon" alt=""></a>
+				
 			</div>
 			<div class="col-sm-4">
+				<a href="/"><img src="../img/logo3.png" class="icon" alt=""></a>
 			</div>
 		</div>
 	</footer>
