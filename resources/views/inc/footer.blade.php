8af27be5ff5ed3dcba61bad7662ff415ec7af94e
Laitia Matagavui <lcavukali@yahoo.com>
2018-06-16 21:56:05 +0100

    share

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -2,7 +2,7 @@
 		<div class="row">
 			<div class="col-sm-4">
 				<h4>Share On Facebook</h4>
-				<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fredblog.cf%2Fimg%2Fmain.png&layout=button_count&size=large&mobile_iframe=true&width=84&height=28&appId" width="84" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+				<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fredblog.cf%2Fabout&layout=button_count&size=large&mobile_iframe=true&width=84&height=28&appId" width="84" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 			</div>
 			<div class="col-sm-4">
 				
