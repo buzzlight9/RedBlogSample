fea3fa77ee28b8820689ae12c505a375e5aff73b
Laitia Matagavui <lcavukali@yahoo.com>
2018-06-16 16:48:04 +0100

    share button

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,17 +1,13 @@
 <footer id="contact" class="container-fluid text-center">
 		<div class="row">
 			<div class="col-sm-4">
-				<h3>Connect</h3>
-				<a href="#" class="fa fa-facebook"></a>
-				<a href="#" class="fa fa-google"></a>
-				<a href="#" class="fa fa-youtube"></a>
-				<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fredblog.cf&layout=button_count&size=small&mobile_iframe=true&width=69&height=20&appId" width="69" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+				<h4>Share On Facebook</h4>
+				<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fredblog.cf%2Fabout%2F&layout=button_count&size=large&mobile_iframe=false&width=84&height=28&appId" width="84" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 			</div>
 			<div class="col-sm-4">
 				<a href="/"><img src="../img/logo3.png" class="icon" alt=""></a>
 			</div>
 			<div class="col-sm-4">
-				<iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fredblog.cf%2Fposts%2F19&layout=button_count&size=large&mobile_iframe=true&width=84&height=28&appId" width="84" height="28" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 			</div>
 		</div>
 	</footer>
