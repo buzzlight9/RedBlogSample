107d8dc0e3c799e741f760ef684d76c23671532b
Laitia Matagavui <lcavukali@yahoo.com>
2018-07-01 15:36:24 +0100

    footer

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -1,7 +1,7 @@
 <footer id="contact" class="container-fluid text-center">
 	<div class="row">
 		<div class="col-sm-4">
-			<h4>SHARE</h4>
+			<h4 class="footerHeading">SHARE</h4>
 		<div class="fb-share-button" data-href="{{Request::url()}}" data-layout="button_count" data-size="large" data-mobile-iframe="true">
 				<a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fredblog.cf%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore fa fa-facebook"></a>
 			</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,6 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta property="og:url"           content="https://redblog.cf/" >
-     <meta property="og:type"          content="website" >
-    <meta property="og:title"         content="RedBlog" >
-    <meta property="og:description"   content="Blogging Platform" >
-    <meta property="og:image"         content="https://redblog.cf/img/redblog2.png" >
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -25,13 +20,13 @@
 </head>
 <body>
         <div id="fb-root"></div>
-        <script>(function(d, s, id) {
-          var js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=241490523101895&autoLogAppEvents=1';
-          fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));</script>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v3.0&appId=241490523101895&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
         @include('inc.navbar')
         @yield('content')
         @include('inc.footer')
